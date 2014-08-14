341a16c3e7c9cfe10ec01caa7dfc6e31220a2ba1
flanr <flanr@live.se>
2014-08-15 00:47:42 +0200

    SoundManager added, Still has 'memleak' and not done

---
+++ 2D-Game basics/SoundManager.cpp
@@ -0,0 +1,196 @@
+// SoundManager.cpp
+#include "SoundManager.h"
+#include <iostream>
+
+
+SoundManager::SoundManager()
+{
+	int m_SoundVolume = 100;
+	int m_MusicVolume = 100;
+	m_SoundBank.reserve(2);
+	m_MusicBank.reserve(2);
+	b_MuteM = false;
+	b_MuteS = false;
+}
+
+SoundManager::~SoundManager()
+{
+}
+
+
+void SoundManager::PlaySound(std::string path)
+{
+	for (SoundsStruct sound : m_SoundBank)
+	{
+		if (sound.path == path)
+		{
+			sound.soundData->play();
+			sound.soundData->setVolume(m_SoundVolume);
+			//			cout << "Sound was found, playing : " << path << endl;
+
+			return;
+		}
+	}
+	//cout << "Sound not found, adding sound : " << path << endl;
+	AddSound(path);
+	PlaySound(path);
+}
+
+void SoundManager::PlayMusic(std::string path)
+{
+
+	if (m_MusicBank.size() > 0)
+	{
+		for (MusicStruct music : m_MusicBank)
+		{
+			if (music.path == path)
+			{
+
+				music.soundHandle->play();
+				music.soundHandle->setVolume(m_MusicVolume);
+				music.soundHandle->setLoop(true);
+				//cout << "Music was found, playing : " << path << endl;
+				return;
+			}
+
+
+		}
+	}
+
+
+	std::cout << "[" << __FILE__ << ":" << __LINE__ << "] Music not found, adding sound : " << path << std::endl;
+	AddMusic(path);
+	PlayMusic(path);
+}
+
+std::vector<SoundsStruct> SoundManager::ReturnSound()
+{
+	return m_SoundBank;
+}
+
+
+
+void SoundManager::StopSound()
+{
+
+}
+
+bool SoundManager::Initialize(std::string directory)
+{
+	m_directory = directory;
+	
+
+	return true;
+}
+
+void SoundManager::SetMusicVolume(int value)
+{
+	m_SoundVolume = value;
+	for (MusicStruct music : m_MusicBank)
+	{
+		music.soundHandle->setVolume(value);
+	}
+}
+
+void SoundManager::SetSoundVolume(int value)
+{
+	m_MusicVolume = value;
+	m_SoundVolume = value;
+	for (SoundsStruct sound : m_SoundBank)
+	{
+		sound.soundData->setVolume(value);
+	}
+}
+
+
+int SoundManager::GetMusicVolume()
+{
+	return m_MusicVolume;
+}
+
+int SoundManager::GetSoundVolume()
+{
+	return m_SoundVolume;
+}
+
+
+void SoundManager::PauseSound()
+{
+
+}
+
+void SoundManager::AddSound(std::string path)// Memleak
+{
+
+	SoundsStruct soundstruct;
+	sf::Sound* thesound = new sf::Sound;
+	sf::SoundBuffer* thebuffer = new sf::SoundBuffer;
+
+	thebuffer->loadFromFile(m_directory + path);
+
+	thesound->setBuffer(*thebuffer);
+
+	soundstruct.path = path;
+	soundstruct.soundData = thesound;
+	soundstruct.buffer = thebuffer;
+
+	m_SoundBank.push_back(soundstruct);
+
+}
+
+void SoundManager::AddMusic(std::string path)
+{
+
+	MusicStruct musicstruct;
+	sf::Music* themusic = new sf::Music;
+
+	themusic->openFromFile(m_directory + path);
+	musicstruct.path = path;
+	musicstruct.soundHandle = themusic;
+
+	m_MusicBank.push_back(musicstruct);
+
+}
+void SoundManager::MuteMusic()
+{
+	if (b_MuteM)
+	{
+		SetMusicVolume(OldVolMusic);
+
+		b_MuteM = false;
+	}
+	else
+	{
+		OldVolMusic = GetMusicVolume();
+
+		SetMusicVolume(0);
+		b_MuteM = true;
+	}
+
+
+
+}
+void SoundManager::MuteSound()
+{
+	if (b_MuteS)
+	{
+
+
+
+	}
+	else
+	{
+
+	}
+	SetSoundVolume(0);
+}
+
+void SoundManager::StopMusic()
+{
+	for (MusicStruct music : m_MusicBank)
+	{
+		music.soundHandle->stop();
+	}
+
+}
+
---
+++ 2D-Game basics/SoundManager.h
@@ -0,0 +1,52 @@
+// SoundManager.h
+#pragma once
+#include <SFML\Audio.hpp>
+#include <string>
+
+
+struct SoundsStruct
+{
+	std::string path;
+	sf::Sound* soundData;
+	sf::SoundBuffer* buffer;
+};
+struct MusicStruct
+{
+	std::string path;
+	sf::Music* soundHandle;
+};
+
+
+class SoundManager
+{
+public:
+	SoundManager();
+	~SoundManager();
+	bool Initialize(std::string path);
+	void PlaySound(std::string path);
+	void PlayMusic(std::string path);
+
+	void StopSound();
+	void StopMusic();
+	void SetMusicVolume(int value);
+	void SetSoundVolume(int value);
+	int GetMusicVolume();
+	int GetSoundVolume();
+	void PauseSound();
+	std::vector<SoundsStruct> ReturnSound();
+	void MuteSound();
+	void MuteMusic();
+
+private:
+	std::vector<SoundsStruct> m_SoundBank;
+	std::vector<MusicStruct> m_MusicBank;
+	int m_SoundVolume;
+	int m_MusicVolume;
+	std::string m_directory;
+	void AddSound(std::string path);
+	void AddMusic(std::string path);
+	int OldVolMusic, OldVolSound;
+	bool b_MuteM, b_MuteS;
+
+};
+
